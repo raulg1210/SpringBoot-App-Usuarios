9bc924bec3be8b5a6d3906621573f134489c288f
raulg1210 <raulgarcianieto02@gmail.com>
2023-02-08 19:19:33 +0100

    Hasta minuto 01:26:00 del curso (crea Usuario y devuelve pasando id, no accede a BD)

---
+++ src/main/java/com/periflus/demo/controllers/UsuarioController.java
@@ -0,0 +1,21 @@
+package com.periflus.demo.controllers;
+
+import com.periflus.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class UsuarioController {
+    @RequestMapping(value="usuario/{id}")
+    public Usuario prueba(@PathVariable Long id){
+        Usuario usuario=new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellidos("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
+        return usuario;
+    }
+}
