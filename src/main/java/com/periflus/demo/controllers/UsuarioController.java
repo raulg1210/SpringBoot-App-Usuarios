9c52c18adc76e32c176cdb80fceacab9a07d74be
raulg1210 <raulgarcianieto02@gmail.com>
2023-02-09 19:39:54 +0100

    Hasta minuto 01:55:25 ( backend devuelve lista de usuarios (sin acceder a BD, para ello modificamos usuarios.js), frontend muestra la lista en una tabla)

--- src/main/java/com/periflus/demo/controllers/UsuarioController.java
+++ src/main/java/com/periflus/demo/controllers/UsuarioController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 @RestController
 public class UsuarioController {
     @RequestMapping(value="usuario/{id}")
-    public Usuario prueba(@PathVariable Long id){
+    public Usuario getUsuario(@PathVariable Long id){
         Usuario usuario=new Usuario();
         usuario.setId(id);
         usuario.setNombre("Lucas");
@@ -17,5 +18,34 @@ public class UsuarioController {
         usuario.setEmail("lucasmoy@hotmail.com");
         usuario.setTelefono("234234234");
         return usuario;
+    }
+    @RequestMapping(value="usuarios")
+    public List<Usuario> getUsuarios(){
+        List<Usuario> usuarios=new ArrayList<>();
+        Usuario usuario=new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Lucas");
+        usuario.setApellidos("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
+        usuarios.add(usuario);
+
+        usuario=new Usuario();
+        usuario.setId(2L);
+        usuario.setNombre("Lucas2");
+        usuario.setApellidos("Moy2");
+        usuario.setEmail("lucasmoy2@hotmail.com");
+        usuario.setTelefono("12");
+        usuarios.add(usuario);
+
+        usuario=new Usuario();
+        usuario.setId(3L);
+        usuario.setNombre("Lucas3");
+        usuario.setApellidos("Moy3");
+        usuario.setEmail("lucasmoy3@hotmail.com");
+        usuario.setTelefono("23");
+        usuarios.add(usuario);
+
+        return usuarios;
     }
 }
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,5 +1,28 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  alert("Esto es una prueba");
+  cargaUsuarios();
   $('#usuarios').DataTable();
 });
+
+async function cargaUsuarios(){
+  const request = await fetch('usuarios', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+  const usuarios = await request.json();
+  let listadoHTML='';
+  for (let usuario of usuarios){
+    let usuarioHTML= '<tr><td>'+usuario.id+
+                     '<td>'+usuario.nombre+
+                     '</td><td>'+usuario.email+
+                     '</td><td>'+usuario.telefono+
+                     '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td><tr>';
+    listadoHTML+=usuarioHTML;
+  }
+  console.log(usuarios);
+
+  document.querySelector('#usuarios tbody').outerHTML=listadoHTML;
+}
